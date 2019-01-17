6c83712c07e4eea210f775d82bc2399d3ac1cbce
remnee <remne@mail.datamupp.com>
2019-01-17 21:01:43 +0100

    ..

--- whiskey_lights/whiskey_lights.ino
+++ whiskey_lights/whiskey_lights.ino
@@ -6,11 +6,13 @@ NeoPixelBus<NeoGrbwFeature, NeoEsp8266Dma800KbpsMethod> strip(NUM_OF_LEDS, 0); /
 NeoPixelBus<NeoGrbwFeature, NeoEsp8266AsyncUart1800KbpsMethod> strip2(NUM_OF_LEDS, 2); // pixelpin is ignored on ESP8266
 
 
-long lastMsg = 0;
+long last = 0;
+long prev = 0;
+
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
   Serial.println("Booting");
@@ -23,17 +25,16 @@ void setup()
 }
 
 uint8_t r = 0;
-uint8_t g = 25;
+uint8_t g = 10;
 uint8_t b = 0;
 
-long prevTime = 0;
 
 void loop()
 {
   long now = millis();
 
-  r = (r+1) % 50;
-  g = (g+1) % 50;
+  r = (r+1) % 20;
+  g = (g+1) % 20;
   for (uint i = 0; i < NUM_OF_LEDS; i++)
   {
     strip.SetPixelColor(i, RgbwColor(0, g, 0));
@@ -44,18 +45,18 @@ void loop()
   strip2.Show();
 
   {
-    Serial.printf("loop: %lu \n", now - prevTime);
-    prevTime = now;
+    Serial.printf("loop: %lu \n", now - prev);
+    prev = now;
   }
     
-  if (now - lastMsg >= 2000)
+  if (now - last >= 2000)
   {
-    lastMsg = now;
+    last = now;
 
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.printf("%lu\n", now);
   }
-  else if (now - lastMsg > 1000)
+  else if (now - last > 1000)
   {
     digitalWrite(LED_BUILTIN, LOW);
   }
